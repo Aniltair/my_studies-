99e730888f795b0fd5fe4d16921d9b8672952936
Aniltair <aniltair14@gmail.com>
2023-02-02 14:34:43 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'lista_de_compras/master'.
    
     Changes to be committed:
    	modified:   geckodriver.log
    	renamed:    raspador/raspador_de_dados.py -> raspador/exemplo_de_po.py
    	renamed:    raspador/raspador_de_dados_2.py -> raspador/po_que_navega_google.py

--- raspador/po_que_navega_google.py
+++ raspador/po_que_navega_google.py
@@ -8,26 +8,30 @@ class Google:
         self.url = 'http://google.com.br'
     def navigate(self):
         self.driver.get(self.url) 
-
-
 def pesquisar():
     bar =  ff.find_element(By.CLASS_NAME, "gLFyf")
     pesquisa =  bar.send_keys(input('O que deseja pesquisar? '))
     time.sleep(1)
     clicar()
     return pesquisa
-
+def pesquisa_com_sorte():
+    bar =  ff.find_element(By.CLASS_NAME, "gLFyf")
+    pesquisa =  bar.send_keys(input('O que deseja pesquisar? '))
+    time.sleep(1)
+    clicar_sorte()
+    return pesquisa_com_sorte
 def clicar():
     clk = ff.find_element(By.CLASS_NAME, 'gNO89b').click()
     return clk
 def clicar_sorte():
-    clk_sorte = ff.find_element(By.CLASS_NAME, 'RNmpXc').click
+    clk_sorte = ff.find_element(By.CLASS_NAME, 'RNmpXc').click()
     return clk_sorte
+
+#ff.quit() fecha o navegador
+
 ff = webdriver.Firefox()
 g = Google(ff)
 g.navigate()
-pesquisar()
-
 
 
 
